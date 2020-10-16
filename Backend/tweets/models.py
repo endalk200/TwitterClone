5e4ff92559ac00692e9cb8a4f0cec6aad2f3c0f5
endalk2 <eb808826@gmail.com>
2020-10-16 19:07:43 +0300

    created Tweets model

--- Backend/tweets/models.py
+++ Backend/tweets/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Tweets(models.Model):
+    content = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # TODO: add additional fields
+
+    objects = models.Manager()
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self, *args, **kwargs) -> str:
+        """
+        string representation of the class
+        """
+        return f"Tweet ID: { self.pk } | Tweet Timestamp: { self.timestamp }"
