8e982fcc006084c8df1b9b10e037f9a92db6157d
endalk2 <eb808826@gmail.com>
2020-10-17 21:29:46 +0300

    added Header and Navbar component

---
+++ Frontend/src/components/common/Navbar.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+
+
+export default class Navbar extends React.Component {
+
+    render() {
+
+        return(
+            <h1 className="text-center">Navbar</h1>
+        )
+    }
+}
