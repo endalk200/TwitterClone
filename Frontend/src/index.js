f8a813bf2ad929c8f1cc348103440f714f690537
endalk2 <eb808826@gmail.com>
2020-10-17 21:11:11 +0300

    imported sass and js files compiled from bootrsap 5 and mdb 5 source code

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+import "./static/scss/style.scss"
+import "./static/js/bundle.js"
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
