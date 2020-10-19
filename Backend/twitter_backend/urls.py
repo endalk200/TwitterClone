52a815aacd4b57f6b4ca8d753571aa68861f2906
endalk2 <eb808826@gmail.com>
2020-10-19 21:14:00 +0300

    registered auth API endpoints

--- Backend/twitter_backend/urls.py
+++ Backend/twitter_backend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('API/', include('tweets.urls'))
+    path('API/', include('tweets.urls')),
+    path('API/auth/', include('__auth.urls'))
 ]
